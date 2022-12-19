bde52289301096de39652d2b0b6b47c024ae06ae
devkudasov <dev.kudasov@gmail.com>
2022-12-19 22:50:32 +0300

    feat(nodejs-ztm): Simple http server

---
+++ nodejs_ztm/http-server/index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const FRIENDS = [
+  {
+    id: 0,
+    name: 'Nikola Tesla',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Albert Einstein',
+  },
+];
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/friends') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+    res.end(JSON.stringify());
+  } else if (req.url === '/messages') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li>Hello Issac!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
