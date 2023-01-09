884cbf573a81ba042f0a359122034f153021ba43
devkudasov <dev.kudasov@gmail.com>
2023-01-09 23:03:16 +0300

    feat(nodejs_ztm): Express routers

--- nodejs_ztm/express-project/server.js
+++ nodejs_ztm/express-project/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const messagesController = require('./controllers/messages.controller');
-const friendsController = require('./controllers/friends.controller');
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
@@ -10,17 +10,13 @@ app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.post('/friends', friendsController.postFriend);
-app.get('/friends', friendsController.getFriendList);
-app.get('/friends/:friendId', friendsController.getFriend);
-
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessage);
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
