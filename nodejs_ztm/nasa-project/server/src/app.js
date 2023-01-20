68ee073ed89a25d8466a78d9dfe3934f3e034f58
devkudasov <dev.kudasov@gmail.com>
2023-01-20 23:29:15 +0300

    feat(nodejs_ztm): launches api

--- nodejs_ztm/nasa-project/server/src/app.js
+++ nodejs_ztm/nasa-project/server/src/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const morgan = require('morgan');
 
 const planersRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -18,8 +19,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/planets', planersRouter);
+app.use('/launches', launchesRouter);
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
